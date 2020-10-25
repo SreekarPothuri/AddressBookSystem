287f98dde73af8e405417b265f3d6826eebf7e6f
Your Name <you@example.com>
2020-10-25 10:04:11 +0530

    Added: Contact details

--- src/com/addressBook/model/Person.java
+++ src/com/addressBook/model/Person.java
@@ -1,17 +1,78 @@
 package com.addressBook.model;
 
 public class Person {
-	private String firstName;
-	private String lastName;
-	private Address address;
-	private String mobileNum;
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public  String state;
+	public String mobileNum;
+	public String zipCode;
 	
-	private Person(){}
-    
-	Person(String firstName, String lastName,Address address, String MobileNumber){
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.mobileNum = mobileNum;
-    }
+	public Person(String firstName, String lastName,String address,String city,String state,String mobileNum,String zipCode)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.mobileNum = mobileNum;
+		this.zipCode = zipCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getMobileNum() {
+		return mobileNum;
+	}
+
+	public void setMobileNum(String mobileNum) {
+		this.mobileNum = mobileNum;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
 }
